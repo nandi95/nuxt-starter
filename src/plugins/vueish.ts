df6ad33fbd813ea95dbcf695ca98974dd4c689a1
Nandor Kraszlan <nandor.kraszlan@gmail.com>
2022-09-21 20:18:59 +0100

    updated dependencies

--- src/plugins/vueish.ts
+++ src/plugins/vueish.ts
@@ -1,9 +1,7 @@
 import { defineNuxtPlugin } from '#app';
-// @ts-expect-error
-import Vueish from '@karnama/vueish';
+import { createUILibrary } from '@karnama/vueish';
 import '../../node_modules/@karnama/vueish/dist/style.css';
 
 export default defineNuxtPlugin(nuxt => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-    nuxt.vueApp.use(Vueish);
+    nuxt.vueApp.use(createUILibrary());
 });
